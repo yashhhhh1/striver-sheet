25efdcc6d5253f11048579a665784697a5d6f319
yashhhhh1 <yash8511132161@gmail.com>
2023-06-15 22:08:14 +0530

    step  started

---
+++ Step 1/step1.2/Q-3.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int row=5,col=5;
+
+    for (int i = 1; i <= row; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout<<j;
+        }
+        cout<<endl;
+        
+    }
+    
+}
---
+++ Step 1/step1.2/Q-4.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int row=5,col=5;
+
+ for (int i = 0; i < row; i++)
+ {
+    for (int j = 0; j < i; j++)
+    {
+        cout<<i;
+    }
+
+    cout<<endl;
+    
+ }
+ 
+}
