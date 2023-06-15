94f75d40b96dc39bd7c8bcbb36af1ff93ef85536
yashhhhh1 <yash8511132161@gmail.com>
2023-06-15 22:03:27 +0530

    step-1.2 start

---
+++ Step 1/step1.2/Q-1.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int row=5,col=5;
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            cout<<"* ";
+        }
+        cout<<endl;
+        
+    }
+    
+}
+
+
+/*
+* * * * * 
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+*/
