70516d64544f1fb063bc57479dad5eb185e74c45
yashhhhh1 <yash8511132161@gmail.com>
2023-06-24 23:24:49 +0530

    step 1.6

---
+++ Step 1/step 1.6/1.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void sol1(int n,int arr[]){
+
+  unordered_map<int,int>mp;
+
+    for (int i = 0; i < n; i++)
+    {
+       mp[arr[i]]++;
+    }
+    cout<<endl;
+    for(auto i:mp){
+        cout<<i.first<<" "<<i.second<<endl;
+    }
+    
+
+}
+
+int main()
+ {
+    
+    int arr[]={1,2,1,2,1,5};
+    int n= sizeof(arr)/sizeof(arr[0]);
+    sol1(n,arr);
+    return 0;
+ }
