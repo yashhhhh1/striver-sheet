cd6d60fcf9449965706f285017bee7b5957f9dcd
yashhhhh1 <yash8511132161@gmail.com>
2023-07-01 14:28:49 +0530

    new question add

--- step 3/step 3.1/13.cpp
+++ step 3/step 3.1/13.cpp
@@ -40,13 +40,52 @@ int sol2(int arr[], int n, int k){
     return len;
 }
 
+void sol3(int arr[], int n ,int sum ){
+    map<int , int> presum;
+    int cursum=0;
+    int maxLen=0;
+
+    for (int i = 0; i < n; i++)
+    {
+        cursum += arr[i];
+        
+
+        if (cursum == sum)
+        {
+            maxLen= max(maxLen,i+1);
+           
+        }
+
+        int rem = cursum - sum;
+
+        if(presum.find(rem) != presum.end()){
+            int len = i - presum[rem];
+            maxLen=max(maxLen,len);
+        }
+
+        if(presum.find(cursum) != presum.end()){
+            presum[cursum]=i;
+        }
+        
+    }
+    cout<<maxLen<<" ";
+
+}
+
 int main()
  {
     int arr[]={2,3,5,1,9};
     int n= sizeof(arr)/sizeof(arr[0]);
     int sum=10;
+
+   
+    int ans=sol1(arr,n,sum);
+    cout<<"sol1 ans is : "<<ans<<" "<<endl;
+
     int a=sol2(arr,n,sum);
-    cout<<a<<" ";
+    cout<<"sol2 ans is : "<<a<<" "<<endl;
+
+    sol3(arr,n,sum);
      return 0;
  }
 
